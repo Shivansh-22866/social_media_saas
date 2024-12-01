d587e90c5050aaf2fb3923d8b5febdf41a566b4c
Shivansh-22866 <shivanshp0418@gmail.com>
2024-12-02 03:28:28 +0530

    Gemini API integration, added function to save and fetch content

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
             quantity: 1,
           },
         ],
-        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/?success=true`,
+        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/generate?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/pricing`,
         client_reference_id: userId,
       });
